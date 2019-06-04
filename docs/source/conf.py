ee6d66fd45f76ab443b918dfb3532c1128b45878
Syncrow <valehue@hotmail.fr>
2019-06-04 17:08:39 +0200

    again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,6 @@ copyright = '2019, Valentin HUE'
 author = 'Valentin HUE'
 
 master_doc = 'index'
-html_theme = 'sphinx_rtd_theme'
 
 # The full version, including alpha/beta/rc tags
 release = '0'
@@ -50,7 +49,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
